860d8045909a8379e67641fd430460fb816879b3
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-16 18:12:39 -0500

    use django form

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,10 +1,21 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
+from django.conf import settings
 # Create your models here.
+from .validators import validate_content
 
 class Tweet(models.Model):
-	content = models.TextField()
-	
+	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+	content = models.CharField(max_length=140, validators=[validate_content])
+	sex = models.CharField(max_length=10)  
+	updated = models.DateTimeField(auto_now_add=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
 	def __str__(self):
 		return str(self.content)
 
+	# def clean(self, *args, **kwargs):
+	# 	content = self.content
+	# 	if content == "abc":
+	# 		raise ValidationError("Cannot be abc")
+	# 	return super(Tweet, self).clean(*args,**kwargs)
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
-
+from django import forms
+from django.shortcuts import render, get_object_or_404
+from django.forms.utils import ErrorList
+from django.views.generic import DetailView, ListView, CreateView
+from .models import Tweet
+from .mixins import FormUserNeededMixin
 # Create your views here.
+
+from .forms import TweetModelForm
+
+class TweetCreateView(FormUserNeededMixin, CreateView):
+	# queryset = Tweet.objects.all()
+	form_class = TweetModelForm
+	template_name = 'tweets/create_view.html'
+	success_url = '/tweet/create/'
+	# fields = ['user','content']
+
+class TweetDetailView(DetailView):
+	# template_name = "tweets/detail_view.html"
+	queryset = Tweet.objects.all()
+
+	def get_object(self):
+		print(self.kwargs)
+		pk = self.kwargs.get("pk")
+		return Tweet.objects.get(id=pk)
+
+class TweetListView(ListView):
+	# template_name = "tweets/list_view.html"
+	queryset = Tweet.objects.all()
+
+# def tweet_detail_view(request, id=1):
+# 	obj = Tweet.objects.get(id=id) 
+# 	print (obj)
+# 	context = {
+# 		"object": obj
+# 	}
+# 	return render(request,"tweets/detail_view.html",context)
+
+# def tweet_list_view(request):
+# 	queryset = Tweet.objects.all()
+# 	print (queryset)
+# 	context = {
+# 		"object_list": queryset
+# 	}
+# 	return render(request,"tweets/list_view.html",context)
