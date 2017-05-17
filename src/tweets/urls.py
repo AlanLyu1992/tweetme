0cff06f1ac15d76d4c7edd307b06a5fa2186b89d
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-17 11:26:32 -0500

    update view

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from .views import TweetCreateView, TweetListView, TweetDetailView
+from .views import TweetCreateView, TweetListView, TweetDetailView, TweetUpdateView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', TweetListView.as_view(), name='list'), #/tweet
     url(r'^create/$', TweetCreateView.as_view(), name='create'), #/tweet/create
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),	# /tweet/1/
+    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),	# /tweet/1/
 
 ]
 
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,28 +1,38 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django import forms
 from django.shortcuts import render, get_object_or_404
 from django.forms.utils import ErrorList
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
 from .models import Tweet
-from .mixins import FormUserNeededMixin
+from .mixins import FormUserNeededMixin, UserOwnerMixin
 # Create your views here.
 
 from .forms import TweetModelForm
 
-class TweetCreateView(FormUserNeededMixin, CreateView):
+class TweetCreateView(LoginRequiredMixin, FormUserNeededMixin, CreateView):
 	# queryset = Tweet.objects.all()
 	form_class = TweetModelForm
 	template_name = 'tweets/create_view.html'
 	success_url = '/tweet/create/'
+	login_url = '/admin/'
 	# fields = ['user','content']
+
+class TweetUpdateView(LoginRequiredMixin,UserOwnerMixin, UpdateView):
+	form_class = TweetModelForm
+	success_url = '/tweet/'
+	template_name = 'tweets/update_view.html'
+	queryset = Tweet.objects.all()
+
+
 
 class TweetDetailView(DetailView):
 	# template_name = "tweets/detail_view.html"
 	queryset = Tweet.objects.all()
 
-	def get_object(self):
-		print(self.kwargs)
-		pk = self.kwargs.get("pk")
-		return Tweet.objects.get(id=pk)
+	# def get_object(self):
+	# 	print(self.kwargs)
+	# 	pk = self.kwargs.get("pk")
+	# 	return Tweet.objects.get(id=pk)
 
 class TweetListView(ListView):
 	# template_name = "tweets/list_view.html"
