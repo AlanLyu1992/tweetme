1dc8047621a90490e9756b689620e7ef4d009364
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-17 15:56:26 -0500

    use bootstrap columns

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django import forms
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.forms.utils import ErrorList
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
@@ -42,6 +43,20 @@ class TweetDetailView(DetailView):
 class TweetListView(ListView):
 	# template_name = "tweets/list_view.html"
 	queryset = Tweet.objects.all()
+	def get_queryset(self, *args, **kwargs):
+		qs = Tweet.objects.all()
+		print(self.request.GET)
+		query = self.request.GET.get("q", None)
+		if query is not None:
+			qs = qs.filter(
+				Q(content__icontains=query)	|
+				Q(user__username__icontains=query)
+				) 
+		return qs
+
+	def get_context_data(self,*args,**kwargs):
+		context = super(TweetListView, self).get_context_data(*args,**kwargs)
+		return context
 
 # def tweet_detail_view(request, id=1):
 # 	obj = Tweet.objects.get(id=id) 
