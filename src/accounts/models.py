43f034b8f469e57f62eef8c6021d54b3e723f39f
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-21 17:26:24 -0500

    implement followers

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile') #user.profile
+	following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
+	# user.profile.following -- users i follow
+	# user.followed_by -- users that follow me
+	def __str__(self):
+		return str(self.following.all().count())
