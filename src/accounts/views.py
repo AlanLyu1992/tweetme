77a3bcc9d5b8d42f944aff4d9a83d155e20cd173
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-22 13:31:26 -0500

    use UserProfile Manager

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView
 from django.contrib.auth import get_user_model
+from django.views import View
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 User = get_user_model()
@@ -10,3 +12,8 @@ class UserDetailView(DetailView):
 
 	def get_object(self):
 		return get_object_or_404(User, username__iexact=self.kwargs.get("username"))
+
+
+class UserFollow(View):
+	def get(self, username):
+		return redirect
