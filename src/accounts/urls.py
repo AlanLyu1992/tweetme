556d60cc0c89709efc86fe6360f874c531b0f6db
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-22 14:07:19 -0500

    implements follow

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
-from .views import UserDetailView
+from .views import UserDetailView, UserFollowView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
@@ -8,6 +8,7 @@ urlpatterns = [
     # url(r'^search/$', TweetListView.as_view(), name='list'), #/tweet/search
     # url(r'^create/$', TweetCreateView.as_view(), name='create'), #/tweet/create
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),	# /tweet/1/
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),	# /tweet/1/
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),	# /tweet/1/update
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),	# /tweet/1/delete
 
