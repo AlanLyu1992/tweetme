f4b58022c5f55679cb0d54ae074af711be5ee593
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-24 10:23:12 -0500

    hashtag module

--- src/hashtags/models.py
+++ src/hashtags/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from tweets.models import Tweet
+from django.urls import reverse_lazy
 # Create your models here.
 
 class HashTag(models.Model):
@@ -11,3 +12,6 @@ class HashTag(models.Model):
 
 	def get_tweets(self):
 		return Tweet.objects.filter(content__icontains="#" + self.tag)
+	
+	def get_absolute_url(self):
+		return reverse_lazy("hashtag", kwargs={"hashtag":self.tag})
