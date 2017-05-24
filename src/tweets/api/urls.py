8b2524709878314418310f04e5fe520623d92000
AlanLyu1992 <lyu.y@wustl.edu>
2017-05-24 12:55:50 -0500

    retweet with jquery

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
-from .views import TweetListAPIView, TweetCreateAPIView
+from .views import TweetListAPIView, TweetCreateAPIView, RetweetAPIView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     # url(r'^$', RedirectView.as_view(url="/")), #/tweet/
     url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet/ 
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), #/api/tweet/create
+    url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='detail'),	# /tweet/1/
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),	# /tweet/1/
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),	# /tweet/1/update
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),	# /tweet/1/delete
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,10 +1,23 @@
 from django.db.models import Q
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 from .pagination import StandardResultsPagination
+
+class RetweetAPIView(APIView):
+	permission_classes = [permissions.IsAuthenticated]
+	def get(self,request,pk,format=None):
+		tweet_qs = Tweet.objects.filter(pk=pk)
+		if tweet_qs.exists() and tweet_qs.count() ==1:
+				new_tweet = Tweet.objects.retweet(request.user,tweet_qs.first())
+				if new_tweet is not None:
+					data = TweetModelSerializer(new_tweet).data
+					return Response(data)
+		return Response("不允许啊",status=400) 
 
 class TweetCreateAPIView(generics.CreateAPIView):
 	serializer_class = TweetModelSerializer
